4388f52cf54a63d8534b8c75b59a502fb2398026
Juliana Ferreira <julianafercavalcante@hotmail.com>
2023-08-31 22:07:38 -0300

    consultas implementadas

--- src/appconsole/Consultar.java
+++ src/appconsole/Consultar.java
@@ -1,10 +1,3 @@
-/**********************************
- * IFPB - Curso Superior de Tec. em Sist. para Internet
- * POB - Persistencia de Objetos
- * Prof. Fausto Ayres
- *
- */
-
 package appconsole;
 
 import java.util.List;
@@ -14,54 +7,47 @@ import com.db4o.query.Candidate;
 import com.db4o.query.Evaluation;
 import com.db4o.query.Query;
 
-import modelo.Aluguel;
-import modelo.Carro;
+import modelo.Registro;
+import modelo.Veiculo;
 
 public class Consultar {
 	protected ObjectContainer manager;
 
 	public Consultar() {
 		try {
-			manager = Util.conectarBanco();  // TERCEIRA CONSULTA USA FILTER
+			manager = Util.conectarBanco();
+			
 			
 			System.out.println("consultas... \n");
-			System.out.println("\nAlugueis de carro palio ordenados por valor");
-			Query q;
+			
+			System.out.println("\nQuais os registros da data 11/02/2023");
+			Query q = manager.query();
+			q.constrain(Registro.class);
+			q.descend("datahora").constrain("11/02/2023").contains();  //condição
+			q.descend("id").orderAscending();	//ordenação
+			List<Registro> resultados = q.execute();
+			for(Registro r : resultados)
+				System.out.println(r);
+			
+		
+			System.out.println("\nQuais os veiculos contendo registro na data 12/02/2023");
 			q = manager.query();
-			q.constrain(Aluguel.class);
-			q.descend("carro").descend("modelo").constrain("palio");  //condi��o
-			q.descend("carro").descend("placa").orderAscending();	//ordena��o
-			List<Aluguel> resultados = q.execute();
-			for(Aluguel a : resultados)
-				System.out.println(a);
+			q.constrain(Veiculo.class);
+			q.descend("registros").descend("datahora").constrain("12/02/2023").contains();  //condição
+			q.descend("placa").orderAscending();	//ordenação
+			List<Veiculo> resultados2 = q.execute();
+			for(Veiculo v : resultados2)
+				System.out.println(v);
 			
 			
-			System.out.println("\nAlugueis nao finalizados");
+			System.out.println("\nQuais os veículos contendo mais de 1 registro");
 			q = manager.query();
-			q.constrain(Aluguel.class);
-			q.descend("finalizado").constrain(false);
-			List<Aluguel> resultados2 = q.execute();
-			for(Aluguel a : resultados2)
-				System.out.println(a);
-			
-			System.out.println("\nCarros que possuem alugueis de 9 dias");
-			q = manager.query();
-			q.constrain(Carro.class);
-			q.descend("alugueis").descend("dias").constrain(9);
-			List<Carro> resultados3 = q.execute();
-			for(Carro c : resultados3)
-				System.out.println(c);
-			
-			System.out.println("\nCarros que possuem 1 alugueis");
-			q = manager.query();
-			q.constrain(Carro.class);
-			q.constrain(new Filtro1());
-			List<Carro> resultados4 = q.execute();
-			for(Carro c : resultados4)
-				System.out.println(c);
-			
-			
-			//System.out.println("\nClientes que possuem 2 alugueis");
+			q.constrain(Veiculo.class);
+			q.constrain( new Filtro1() );
+			List<Veiculo> resultados3 = q.execute();
+			for(Veiculo v : resultados3)
+				System.out.println(v);
+		
 			
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
@@ -79,26 +65,10 @@ public class Consultar {
 //classe interna
 class Filtro1 implements Evaluation {
 	public void evaluate(Candidate candidate) {
-		Carro car = (Carro) candidate.getObject();
-		if(car.getAlugueis().size()== 1) 
+		Veiculo veiculo = (Veiculo) candidate.getObject();
+		if(veiculo.getRegistros().size() > 1) 
 			candidate.include(true); 
-		else		
-			candidate.include(false);
-	}
-}
-
-class Filtro2 implements Evaluation {
-	public void evaluate(Candidate candidate) {
-		Carro car = (Carro) candidate.getObject();
-		//verificar se todos os alugueis do carro estao finalizados
-		boolean finalizado = true;
-		for(Aluguel a : car.getAlugueis())
-			if(!a.isFinalizado())
-				finalizado=false;
-				
-		if(finalizado)
-			candidate.include(true);
-		else		
+		else
 			candidate.include(false);
 	}
 }
